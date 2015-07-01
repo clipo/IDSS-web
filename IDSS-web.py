4cc6a2e6e8e77068ad0c920111e608ec68810c86
Carl Lipo <clipo@csulb.edu>
2015-07-01 13:47:14 -0700

    added celery task stuff in

--- IDSS-web.py
+++ IDSS-web.py
@@ -1,6 +1,6 @@
 
 from flask import render_template
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
 import os.path
 import os
 from seriation import IDSS
@@ -12,6 +12,10 @@ import sys
 import zipfile
 import subprocess
 import re
+import logging as log
+import time
+import random
+from celery import Celery  # uses RabbitMQ
 
 DATABASE_NAME = './database/idssProcessing.sqlite'
 UPLOAD_FOLDER = '/var/www/uploads/'
@@ -21,10 +25,14 @@ MAX_ASSEMBLAGES = 15
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+log.basicConfig(format=FORMAT)
+
 
 @app.route('/')
 def index():
@@ -49,52 +57,83 @@ def upload():
 
     if not errors:
         # If there are no errors, create a dictionary containing all the entered
-        data = {'name' : name,
+        data = {    'name' : name,
                     'email' : email,
-                    'university' : university
-                    }
+                    'university' : university,
+                    'filename': file.filename
+                }
     else:
         return render_template('index.html', error=errors)
 
     if file and allowed_file(file.filename):
         jobname=create_job()
-        print jobname
+        log.debug('Created jobname: %s', jobname)
         filepath = UPLOAD_FOLDER+str(jobname)+"/"
-        print filepath
+        log.debug('Filepath is: %s', filepath)
         if not os.path.exists(filepath):
                 os.makedirs(filepath)
         filename=(os.path.join(filepath, file.filename))
-        print filename
+        log.debug('Filename is %s: ', filename)
         file.save(filename)
         problems = check_file_for_valid_input(filename)
-        print "problems: ", problems
+        log.debug("Problems: %s", problems)
         if problems is not "":
-            print "Uh oh. Problems."
+            log.debug( 'Uh oh. Problems. Message: %s', problems)
             return render_template('index.html', error=problems, filename=filename, path=filepath, jobname=jobname)
         else:
-            ##run_idss(filename,filepath,jobname)
             return redirect(url_for('/processing',
                                 filename=filename, path=filepath, jobname=jobname))
 
 @app.route('/processing')
-def check_to_see_if_done():
-    filename = request.args['filename']
-    path = request.args['path']
-    resultsfile=filename[:-4]+"-continuity-minmax-by-weight.png"
-    if os.path.isfile(resultsfile):
-        zippedpath=zipresults(filename, path)
-    else:
-        return redirect(url_for('/processing', filename=filename, path=path))
-    return zippedpath
+def check_to_see_if_done(filename,path,jobname):
+    task = run_idss.apply_async(filename,path,jobname)
+    return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
 
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
 # directory and show it on the browser, so if the user uploads
 # an image, that image is going to be show after the upload
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
+@app.route('/uploads/<jobname>')
+def uploaded_file(jobname):
+    zipf=zipfile.ZipFile(jobname+".zip","w")
+    zipdir="/var/www/uploads/"+jobname,zipf
+    zipf.close()
     return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+                               zipf)
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = run_idss.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        ## job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def zipresults(filename, path,jobname):
     zipf = zipfile.ZipFile(jobname+".zip", 'w')
@@ -111,20 +150,18 @@ def zipdir(path, ziph):
 
 def check_file_for_valid_input(filename):
     problems = ""
-    print "Checking on file: ", filename, " for errors."
+    log.debug("Checking on file: %s for errors", filename)
     with open(filename, 'rb') as csvfile:
-        print "opening file: ", filename
+        log.debug("opening file: %s", filename)
         lines = file_len(filename)
-        print "number of lines in file: ", lines
+        log.debug("number of lines in file: %d ", lines)
         if lines < 4:
             problems +=  " <li>Number of lines is < 4 which means there are only 2 lines plus header... not enough."
         if lines > MAX_ASSEMBLAGES:
             problems += " <li>Too many assemblages (Max is "+ MAX_ASSEMBLAGES + ")"
-        print "No problem with numbers of lines"
         ## skip the first line (header)
         next(csvfile)
         linenum = 1
-        print "skipped first line (header). "
         reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
         for row in reader:
             linenum +=1
@@ -132,7 +169,7 @@ def check_file_for_valid_input(filename):
             problems += str(check_line_for_format(row))
             #   -- at least 4 rows (can't really do much with anything less
             #   -- no more than 20 rows (for now)
-    print "Problems encountered: ", problems
+    log.debug("Problems encountered: %s ", problems)
     return problems
 
 def file_len(fname):
@@ -140,19 +177,18 @@ def file_len(fname):
         for i, l in enumerate(f):
             pass
     return i + 1
+
 # check to see:
 #   -- that there is a string in the first position
 #   -- more than 1 column of integers
 def check_line_for_format(line):
     problems = ""
-    print "checking that there are enough columns. Number = ", len(line)
+    log.debug( "checking that there are enough columns. Number = %d", len(line))
     ## too few columns
     if len(line)<3:
         problems += " <li> Too few columns ", len(line), " is < 3. "
-
-
     ## check that first column is string
-    print "checking that the first column is a string.  First column: ", line[0]
+    log.debug( "Checking that the first column is a string.  First column: %d", line[0])
     if isinstance(line[0], basestring):
         pass
     else:
@@ -160,7 +196,7 @@ def check_line_for_format(line):
 
 
     ## check that that the other columns are integers
-    print "checking the rest of the columns to see if they are all integers. "
+    log.debug( "checking the rest of the columns to see if they are all integers. ")
     for c in line[1:]:
         if isinstance(int(c),int):
             print c, " is an integer"
@@ -169,7 +205,7 @@ def check_line_for_format(line):
         else:
             print c, " is not an integer"
             problems += " <li> columns of data are not integers. "
-    print "Errors so far: ", problems
+    log.debug("Errors so far: %s ", problems)
     return problems
 
 def create_job():
@@ -195,8 +231,7 @@ def check_to_see_if_processing_is_taking_place(jobname):
         return data
 
     except lite.Error, e:
-
-        print "Error %s:" % e.args[0]
+        log.error("SQLlite Error: %s ", e.args[0])
         return e.args[0]
 
     finally:
@@ -211,11 +246,9 @@ def set_status_to_busy(jobname):
         cur = con.cursor()
         cur.execute('update tblProcessing SET status=?, timestamp=?', ('busy','CURRENT_TIMESTAMP'))
         con.commit()
-
         return True
-
     except lite.Error, e:
-        print "Error %s:" % e.args[0]
+        log.error("SQLlite Error: %s ", e.args[0])
         return e.args[0]
 
     finally:
@@ -231,12 +264,29 @@ def set_status_to_free(jobname):
         con.commit()
         return True
     except lite.Error, e:
-        print "Error %s:" % e.args[0]
+        log.error("SQLlite Error: %s ", e.args[0])
         return e.args[0]
 
     finally:
         if con:
             con.close()
+
+@celery.task(bind=True)
+def long_task(self):
+    action = ['Starting up', 'Processing', 'Grinding away', 'Sweating the details', 'Figuring it out','Chipping away','Chugging along','Still at it']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(action)
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
+
 
 def run_idss(filename, filepath, jobname):
     #set status to busy
