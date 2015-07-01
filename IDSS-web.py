8c9f30cab353fe28d70fe2bfa72e1af301bb0987
Carl Lipo <clipo@csulb.edu>
2015-07-01 11:32:40 -0700

    getting the upload stuff to work and validation to function

--- IDSS-web.py
+++ IDSS-web.py
@@ -11,10 +11,12 @@ import sqlite3 as lite
 import sys
 import zipfile
 import subprocess
+import re
 
 DATABASE_NAME = './database/idssProcessing.sqlite'
-UPLOAD_FOLDER = '/var/www/uploads/data/'
+UPLOAD_FOLDER = '/var/www/uploads/'
 ALLOWED_EXTENSIONS = set(['txt'])
+MAX_ASSEMBLAGES = 15
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -35,16 +37,42 @@ def about():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['filename']
+    name = request.form['name']
+    university= request.form['university']
+    email = request.form['email']
+    errors = ""
+    if not name or not email or not university:
+        errors = "<li>Please enter all the fields."
+
+    if not is_email_address_valid(email):
+        errors +=  "<li>Please enter a valid email address"
+
+    if not errors:
+        # If there are no errors, create a dictionary containing all the entered
+        data = {'name' : name,
+                    'email' : email,
+                    'university' : university
+                    }
+    else:
+        return render_template('index.html', error=errors)
+
     if file and allowed_file(file.filename):
         jobname=create_job()
-        filepath = app.config['UPLOAD_FOLDER']+jobname+"/"
+        print jobname
+        filepath = UPLOAD_FOLDER+str(jobname)+"/"
+        print filepath
+        if not os.path.exists(filepath):
+                os.makedirs(filepath)
         filename=(os.path.join(filepath, file.filename))
+        print filename
         file.save(filename)
         problems = check_file_for_valid_input(filename)
-        if problems is not None:
-            render_template('/index.html', problems=problems)
+        print "problems: ", problems
+        if problems is not "":
+            print "Uh oh. Problems."
+            return render_template('index.html', error=problems, filename=filename, path=filepath, jobname=jobname)
         else:
-            run_idss(filename,filepath,jobname)
+            ##run_idss(filename,filepath,jobname)
             return redirect(url_for('/processing',
                                 filename=filename, path=filepath, jobname=jobname))
 
@@ -83,36 +111,66 @@ def zipdir(path, ziph):
 
 def check_file_for_valid_input(filename):
     problems = ""
-    open(filename)
+    print "Checking on file: ", filename, " for errors."
     with open(filename, 'rb') as csvfile:
+        print "opening file: ", filename
+        lines = file_len(filename)
+        print "number of lines in file: ", lines
+        if lines < 4:
+            problems +=  " <li>Number of lines is < 4 which means there are only 2 lines plus header... not enough."
+        if lines > MAX_ASSEMBLAGES:
+            problems += " <li>Too many assemblages (Max is "+ MAX_ASSEMBLAGES + ")"
+        print "No problem with numbers of lines"
+        ## skip the first line (header)
+        next(csvfile)
+        linenum = 1
+        print "skipped first line (header). "
         reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
         for row in reader:
-            line
-
-    #   -- at least 4 rows (can't really do much with anything less
-    #   -- no more than 20 rows (for now)
+            linenum +=1
+            print "now on line ", linenum
+            problems += str(check_line_for_format(row))
+            #   -- at least 4 rows (can't really do much with anything less
+            #   -- no more than 20 rows (for now)
+    print "Problems encountered: ", problems
     return problems
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 # check to see:
 #   -- that there is a string in the first position
 #   -- more than 1 column of integers
 def check_line_for_format(line):
-    error=0
-    col = line.split(line, '\t')
-    if len(col)<3:
-        error +=1
+    problems = ""
+    print "checking that there are enough columns. Number = ", len(line)
+    ## too few columns
+    if len(line)<3:
+        problems += " <li> Too few columns ", len(line), " is < 3. "
 
-    if isinstance(col[0], basestring):
+
+    ## check that first column is string
+    print "checking that the first column is a string.  First column: ", line[0]
+    if isinstance(line[0], basestring):
         pass
     else:
-        error +=1
+        problems += " <li> First column entry is not a string. "
 
-    for c in col[1:]:
-        if isinstance(c,int):
+
+    ## check that that the other columns are integers
+    print "checking the rest of the columns to see if they are all integers. "
+    for c in line[1:]:
+        if isinstance(int(c),int):
+            print c, " is an integer"
             pass
+
         else:
-            error +=1
-    return error
+            print c, " is not an integer"
+            problems += " <li> columns of data are not integers. "
+    print "Errors so far: ", problems
+    return problems
 
 def create_job():
     return uuid.uuid4()
@@ -189,6 +247,12 @@ def run_idss(filename, filepath, jobname):
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
     return out
+
+def is_email_address_valid(email):
+    """Validate the email address using a regex."""
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
+        return False
+    return True
 
 if __name__ == '__main__':
     app.run()
